659e9a4069094035eca095bdd0443fe4b74e1109
Alexander I.Grafov <grafov@inet.name>
2023-04-26 01:40:06 +0300

    Extend indicators fields based on output from bf-109e-4

--- input/indicators/types.go
+++ input/indicators/types.go
@@ -59,61 +59,74 @@ package indicators
 */
 
 type indicator struct {
-	Valid              bool    `json:"valid"`
-	Type               string  `json:"type"`
-	Speed              float64 `json:"speed"`
-	Pedals1            float64 `json:"pedals1"`
-	Pedals2            float64 `json:"pedals2"`
-	Pedals3            float64 `json:"pedals3"`
-	Pedals4            float64 `json:"pedals4"`
-	Pedals5            float64 `json:"pedals5"`
-	Pedals6            float64 `json:"pedals6"`
-	StickElevator      float64 `json:"stick_elevator"`
-	StickAilerons      float64 `json:"stick_ailerons"`
-	Vario              float64 `json:"vario"`
 	AltitudeHour       float64 `json:"altitude_hour"`
 	AltitudeMin        float64 `json:"altitude_min"`
+	AmmoCounter1       float64 `json:"ammo_counter1"`
+	AmmoCounter1Lamp   float64 `json:"ammo_counter1_lamp"`
+	AmmoCounter2       float64 `json:"ammo_counter2"`
+	AmmoCounter2Lamp   float64 `json:"ammo_counter2_lamp"`
+	AmmoCounter3       float64 `json:"ammo_counter3"`
+	AmmoCounter3Lamp   float64 `json:"ammo_counter3_lamp"`
+	AviahorizonPitch   float64 `json:"aviahorizon_pitch"`
+	AviahorizonRoll    float64 `json:"aviahorizon_roll"`
 	Bank               float64 `json:"bank"`
-	Turn               float64 `json:"turn"`
-	Compass            float64 `json:"compass"`
-	ClockHour          float64 `json:"clock_hour"`
-	ClockMin           float64 `json:"clock_min"`
-	ClockSec           float64 `json:"clock_sec"`
-	ManifoldPressure   float64 `json:"manifold_pressure"`
-	Rpm                float64 `json:"rpm"`
-	OilPressure        float64 `json:"oil_pressure"`
-	OilPressure1       float64 `json:"oil_pressure1"`
-	OilTemperature     float64 `json:"oil_temperature"`
-	HeadTemperature    float64 `json:"head_temperature"`
-	Mixture            float64 `json:"mixture"`
-	Mixture1           float64 `json:"mixture_1"`
-	Fuel               float64 `json:"fuel"`
-	FuelPressure       float64 `json:"fuel_pressure"`
-	Gears              float64 `json:"gears"`
-	GearLampDown       float64 `json:"gear_lamp_down"`
-	GearLampUp         float64 `json:"gear_lamp_up"`
-	GearLampOff        float64 `json:"gear_lamp_off"`
-	Flaps              float64 `json:"flaps"`
-	Throttle           float64 `json:"throttle"`
-	Throttle1          float64 `json:"throttle_1"`
-	Weapon1            float64 `json:"weapon1"`
-	Weapon2            float64 `json:"weapon2"`
-	Weapon3            float64 `json:"weapon3"`
-	PropPitch          float64 `json:"prop_pitch"`
-	Supercharger       float64 `json:"supercharger"`
-	FlapsIndicator     float64 `json:"flaps_indicator"`
-	GearLIndicator     float64 `json:"gear_l_indicator"`
-	GearRIndicator     float64 `json:"gear_r_indicator"`
-	RadiatorLever11    float64 `json:"radiator_lever1_1"`
-	RadiatorLever12    float64 `json:"radiator_lever1_2"`
-	OilRadiatorLever11 float64 `json:"oil_radiator_lever1_1"`
-	OilRadiatorLever12 float64 `json:"oil_radiator_lever1_2"`
 	Blister1           float64 `json:"blister1"`
 	Blister2           float64 `json:"blister2"`
 	Blister3           float64 `json:"blister3"`
 	Blister4           float64 `json:"blister4"`
 	Blister5           float64 `json:"blister5"`
 	Blister6           float64 `json:"blister6"`
+	ClockHour          float64 `json:"clock_hour"`
+	ClockMin           float64 `json:"clock_min"`
+	ClockSec           float64 `json:"clock_sec"`
+	Compass            float64 `json:"compass"`
+	Flaps              float64 `json:"flaps"`
+	FlapsIndicator     float64 `json:"flaps_indicator"`
+	Fuel               float64 `json:"fuel"`
+	Fuel1              float64 `json:"fuel1"`
+	FuelPressure       float64 `json:"fuel_pressure"`
+	GearLIndicator     float64 `json:"gear_l_indicator"`
+	GearLampDown       float64 `json:"gear_lamp_down"`
+	GearLampOff        float64 `json:"gear_lamp_off"`
+	GearLampUp         float64 `json:"gear_lamp_up"`
+	GearRIndicator     float64 `json:"gear_r_indicator"`
+	Gears              float64 `json:"gears"`
+	HeadTemperature    float64 `json:"head_temperature"`
+	ManifoldPressure   float64 `json:"manifold_pressure"`
+	Mixture            float64 `json:"mixture"`
+	Mixture1           float64 `json:"mixture_1"`
+	OilPressure        float64 `json:"oil_pressure"`
+	OilPressure1       float64 `json:"oil_pressure1"`
+	OilRadiatorLever11 float64 `json:"oil_radiator_lever1_1"`
+	OilRadiatorLever12 float64 `json:"oil_radiator_lever1_2"`
+	OilTemperature     float64 `json:"oil_temperature"`
+	Pedals1            float64 `json:"pedals1"`
+	Pedals2            float64 `json:"pedals2"`
+	Pedals3            float64 `json:"pedals3"`
+	Pedals4            float64 `json:"pedals4"`
+	Pedals5            float64 `json:"pedals5"`
+	Pedals6            float64 `json:"pedals6"`
+	PropPitch          float64 `json:"prop_pitch"`
+	PropPitchHour      float64 `json:"prop_pitch_hour"`
+	PropPitchMin       float64 `json:"prop_pitch_min"`
+	RadiatorLever11    float64 `json:"radiator_lever1_1"`
+	RadiatorLever12    float64 `json:"radiator_lever1_2"`
+	Rpm                float64 `json:"rpm"`
+	Speed              float64 `json:"speed"`
+	StickAilerons      float64 `json:"stick_ailerons"`
+	StickElevator      float64 `json:"stick_elevator"`
+	Supercharger       float64 `json:"supercharger"`
+	Throttle           float64 `json:"throttle"`
+	Throttle1          float64 `json:"throttle_1"`
+	Trimmer            float64 `json:"trimmer"`
+	Turn               float64 `json:"turn"`
+	Type               string  `json:"type"`
+	Valid              bool    `json:"valid"`
+	Vario              float64 `json:"vario"`
+	WaterTemperature   float64 `json:"water_temperature"`
+	Weapon1            float64 `json:"weapon1"`
+	Weapon2            float64 `json:"weapon2"`
+	Weapon3            float64 `json:"weapon3"`
 }
 
 type poller interface {
